dff55488f0f83a1c66ff52f38a6141a238d5a0af
Wisse <wissehes@gmail.com>
2020-04-08 10:23:19 +0200

    Added requestsong and stationchanger windows

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-const { app, BrowserWindow, Notification } = require('electron')
+const { app, BrowserWindow, Notification, ipcMain, dialog } = require('electron')
 const path = require('path')
 const { autoUpdater } = require('electron-updater');
 
+let mainWindow;
+
 function createWindow() {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
             width: 500,
             height: 130,
             frame: false,
             transparent: true,
             resizable: false,
+            maximizable: false,
+            fullscreenable: false,
+            hasShadow: false,
             webPreferences: {
-                //preload: path.join(__dirname, 'preload.js'),
                 nodeIntegration: true
             }
         })
@@ -20,8 +24,10 @@ function createWindow() {
     autoUpdater.checkForUpdatesAndNotify();
 }
 
-
-app.whenReady().then(createWindow)
+let tray = null
+app.on("ready", () => {
+    createWindow()
+})
 
 app.on('window-all-closed', function() {
     if (process.platform !== 'darwin') app.quit()
@@ -50,3 +56,28 @@ autoUpdater.on('update-downloaded', () => {
         })
     }
 });
+ipcMain.on('requestsong', (event, args) => {
+    requestSongWindow = new BrowserWindow({
+        parent: mainWindow,
+        width: 600,
+        height: 700,
+        title: "Request songs",
+        backgroundColor: "#000000",
+    })
+    requestSongWindow.loadURL(`https://radio.chickenfm.com/public/${args}/embed-requests`)
+})
+ipcMain.on("stationChanger", () => {
+    stationChangeWindow = new BrowserWindow({
+        parent: mainWindow,
+        width: 400,
+        height: 400,
+        title: "Stations",
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    stationChangeWindow.loadURL(path.join("file://", __dirname, 'html', 'stations.html'), )
+})
+ipcMain.on("changeStation", () => {
+    mainWindow.webContents.send("updateStation")
+})
